4102ca5a82ec83100fbb9c5b53af3226f2dc9212
izanovic <Julian1620@gmail.com>
2017-04-06 13:35:04 +0200

    updating script folder

---
+++ SimulatorSDMgit/Assets/Scripts/AI/CarEngine.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarEngine : MonoBehaviour {
+
+    public Transform path;
+    public float maxSteerAngle = 40f;
+    private List<Transform> nodes;
+    private int currentNode = 0;
+    public WheelCollider wheelFL;
+    public WheelCollider wheelFR;
+
+	// Use this for initialization
+	void Start () {
+        Transform[] pathTransform = path.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+        foreach (Transform t in pathTransform)
+        {
+            if (t != path.transform)
+                nodes.Add(t);
+        }
+    }
+
+    void FixedUpdate() {
+        ApplySteer();
+    }
+    void ApplySteer() {
+        Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
+        float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ SimulatorSDMgit/Assets/Scripts/AI/Path.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Path : MonoBehaviour
+{
+    public Color limecolor;
+    private List<Transform> nodes = new List<Transform>();
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = limecolor;
+        Transform[] pathTransform = GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+        foreach(Transform t in pathTransform)
+        {
+            if (t != transform)
+                nodes.Add(t);
+        }
+
+        for(int i = 0; i < nodes.Count; i++)
+        {
+            Vector3 currentNode = nodes[i].position;
+            Vector3 previousNode = Vector3.zero;
+            if (i > 0)          
+                previousNode = nodes[i - 1].position;      
+            else if ( i == 0 && nodes.Count > 1)
+                previousNode = nodes[nodes.Count - 1].position;
+
+            Gizmos.DrawLine(previousNode,currentNode);
+            Gizmos.DrawWireSphere(currentNode, 0.2f);
+        }
+    }
+
+
+}
